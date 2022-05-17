fac752ca8968000468dd63cad7aa2c07f3d2f4fa
ann-kar <anna.paulina.karpiuk@gmail.com>
2022-05-17 19:45:43 +0200

    refactor: tiles variable

--- src/components/Board/Board.tsx
+++ src/components/Board/Board.tsx
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+
 import { Tile } from "../Tile/Tile";
 import { PawnWrapper } from "../PawnWrapper/PawnWrapper";
 import { Row } from "../Row/Row";
@@ -8,7 +9,7 @@ const StyledBoard = styled.div`
 `;
 
 export const Board = () => {
-  const tiles = new Array(10).fill(0).map((el) => new Array(10).fill(0));
+  const tiles = new Array(10).fill(0).map(() => new Array(10).fill(0));
 
   return (
     <StyledBoard>
@@ -17,8 +18,8 @@ export const Board = () => {
           <Row key={`row-${i}`}>
             {row.map((tile, j) => {
               let isOdd = true;
-              if (((j % 2) && (i % 2)) || (!(j % 2) && !(i % 2))) {
-                isOdd = false
+              if ((j % 2 && i % 2) || (!(j % 2) && !(i % 2))) {
+                isOdd = false;
               }
               return <Tile isOdd={isOdd} key={`tile-${i}-${j}`} />;
             })}
