54ac73e5c56524e707ee8acb88242c5a753bed62
ann-kar <anna.paulina.karpiuk@gmail.com>
2022-05-17 19:46:10 +0200

    refactor: update useRef() type

--- src/components/PawnWrapper/PawnWrapper.tsx
+++ src/components/PawnWrapper/PawnWrapper.tsx
@@ -10,7 +10,7 @@ const StyledPawnWrapper = styled.div`
 `;
 
 export const PawnWrapper = () => {
-  const pawnEl = useRef<any>();
+  const pawnEl = useRef<HTMLDivElement>(null);
   const { setDirection, position, moveRight, moveLeft } = usePawnMovement();
   const [offset, setOffset] = useState<Offset>({ bottom: 0, left: 0 });
 
@@ -30,6 +30,7 @@ export const PawnWrapper = () => {
   }, [position]);
 
   const handleKeyDown = (event: any) => {
+    if (!pawnEl.current) throw Error("pawn element is not assigned");
     pawnEl.current.focus();
     if (event.key === "ArrowUp") {
       if (position.col + 1 <= 9) {
@@ -41,7 +42,7 @@ export const PawnWrapper = () => {
       }
     } else if (event.key === "ArrowLeft") {
       if (position.row - 1 >= 0 && position.col + 1 <= 9) {
-       moveLeft();
+        moveLeft();
       }
     } else if (event.key === "ArrowDown") {
       if (position.col - 1 >= 0) {
