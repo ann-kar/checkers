616a900faed48a8269454cc4997b9aeb447e74f2
ann-kar <anna.paulina.karpiuk@gmail.com>
2022-05-17 18:30:25 +0200

    feat: move pawn with keys

--- src/components/Pawn/Pawn/Pawn.tsx
+++ src/components/Pawn/Pawn/Pawn.tsx
@@ -1,12 +1,78 @@
-import { useRef } from "react";
+import { useEffect, useRef, useState } from "react";
+import { produce } from "immer";
+
 import "./Pawn.css";
 
 export const Pawn = () => {
   const pawnEl = useRef<any>();
+  const [direction, setDirection] = useState("up");
+  const [position, setPosition] = useState({ row: 0, col: 0 });
+  const [translate, setTranslate] = useState({ bottom: 0, left: 0 });
+
+  useEffect(() => {
+    window.addEventListener("keydown", handleKeyDown);
+
+    return () => {
+      window.removeEventListener("keydown", handleKeyDown);
+    };
+  });
+
+  useEffect(() => {
+    setTranslate(
+      produce((draft) => {
+        return {
+          bottom: position.col * 2.1 + "rem",
+          left: position.row * 2.1 + "rem",
+        };
+      })
+    );
+  }, [position]);
 
   const handleKeyDown = (event: any) => {
     pawnEl.current.focus();
+
+    if (event.key === "ArrowUp") {
+      if (position.col + 1 <= 9) {
+        setDirection("up");
+        setPosition(
+          produce((draft) => {
+            return { row: draft.row, col: draft.col + 1 };
+          })
+        );
+      }
+    } else if (event.key === "ArrowRight") {
+      setDirection("right");
+      if (position.row + 1 <= 9) {
+        setPosition(
+          produce((draft) => {
+            return { row: draft.row + 1, col: draft.col };
+          })
+        );
+      }
+    } else if (event.key === "ArrowLeft") {
+      if (position.row - 1 >= 0) {
+        setPosition(
+          produce((draft) => {
+            return { row: draft.row - 1, col: draft.col };
+          })
+        );
+      }
+    } else if (event.key === "ArrowDown") {
+      if (position.col - 1 >= 0) {
+        setPosition(
+          produce((draft) => {
+            return { row: draft.row, col: draft.col - 1 };
+          })
+        );
+      }
+    }
   };
 
-  return <div className="Pawn" ref={pawnEl} onKeyUp={handleKeyDown}></div>;
+  return (
+    <div
+      className="Pawn"
+      style={translate}
+      ref={pawnEl}
+      onKeyUp={handleKeyDown}></div>
+  );
 };
