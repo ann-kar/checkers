856fd359387924966496dfc1358cf4158ecfe403
ann-kar <anna.paulina.karpiuk@gmail.com>
2022-05-17 21:02:42 +0200

    refactor: add move check helpers

---
+++ src/utils/MoveCheck.ts
@@ -0,0 +1,20 @@
+import { IPosition } from "../providers/ContextProvider";
+
+export class MoveCheck {
+
+    static canMoveDown = (position:IPosition) => {
+      return position.row - 1 >= 0;
+    };
+
+    static canMoveUp = (position:IPosition) => {
+      return position.row + 1 <= 9;
+    };
+
+    static canMoveRight = (position:IPosition) => {
+      return position.col + 1 <= 9;
+    };
+
+    static canMoveLeft = (position:IPosition) => {
+      return position.col - 1 >= 0;
+    };
+  }
