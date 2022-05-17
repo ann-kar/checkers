346598a96c4fe1b4bab1ccee618542cea7aa6e22
ann-kar <anna.paulina.karpiuk@gmail.com>
2022-05-17 19:01:43 +0200

    fix: update Board import

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 import "./App.css";
-import { Board } from "./components/Pawn/Board/Board";
+import { Board } from "./components/Board/Board";
 
 function App() {
   return (
